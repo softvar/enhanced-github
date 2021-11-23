8e9800c47bad2bc6c6d246a2037df41ac9bf7975
7db9a <repo.7db9a@gmail.com>
2021-11-23 10:52:39 -0500

    Check pulls index exists in domUtil.

--- src/utils/domUtil.js
+++ src/utils/domUtil.js
@@ -101,7 +101,11 @@ const domUtil = {
   addCopyAndDownloadButton: function() {
     const btnGroup = document.querySelectorAll('.BtnGroup:not(.d-md-none)')[0];
 
-    if (btnGroup && window.location.href && window.location.href.indexOf('blob/' + commonUtil.getBranch()) > -1) {
+    //https://github.com/turbo-src/extension/blob/master/src/inject.js
+    // Previous conditional
+    //if (btnGroup && window.location.href && window.location.href.indexOf('blob/' + commonUtil.getBranch()) > -1) {
+    // New conditional
+    if (btnGroup && window.location.href && window.location.href.indexOf('pulls) > -1) {
       // instantiate copy to clipborad
       new ClipboardLib('.js-file-clipboard'); // eslint-disable-line no-new
 
