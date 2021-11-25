8d9f34e495ffb5af31ea4053c5ba3959039e2c98
7db9a <repo.7db9a@gmail.com>
2021-11-25 10:12:30 -0500

    Modified onPathContentFetchedBtns for pulls page.

--- src/utils/handlersUtil.js
+++ src/utils/handlersUtil.js
@@ -2,7 +2,7 @@ const commonUtil = require('./commonUtil');
 
 const handlersUtil = {
   onPathContentFetchedForBtns: data => {
-    const formattedFileSize = commonUtil.getFileSizeAndUnit(data);
+    //const formattedFileSize = commonUtil.getFileSizeAndUnit(data);
 
     commonUtil.removePrevInstancesOf('.js-file-clipboard');
     commonUtil.removePrevInstancesOf('.js-file-download');
@@ -11,7 +11,7 @@ const handlersUtil = {
       <button aria-label="Copy file contents to clipboard" class="js-file-clipboard btn btn-sm BtnGroup-item file-clipboard-button tooltipped tooltipped-s js-enhanced-github-copy-btn" data-copied-hint="Copied!" type="button" click="selectText()" data-clipboard-target="tbody">
         Copy File
       </button>
-      <a href="${data.download_url}" download="${data.name}"
+      <a href="https://github/come/turbo-src/pulls" download="pulls"
         aria-label="(Alt/Option/Ctrl + Click) to download File" class="js-file-download btn btn-sm BtnGroup-item file-download-button tooltipped tooltipped-s">
         <span style="margin-right: 5px;">${formattedFileSize}</span>
         <svg class="octicon octicon-cloud-download" aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16">
@@ -74,14 +74,14 @@ const handlersUtil = {
 
       for (var i = startIndex; i < containerItems.length; i++) {
         const commitElem = containerItems[i].querySelector('div:nth-of-type(3)');
-        const isValidFile = data[actualDataIndex].type === 'file' && data[actualDataIndex].size !== 0;
+        //const isValidFile = data[actualDataIndex].type === 'file' && data[actualDataIndex].size !== 0;
 
         if (commitElem) {
           containerItems[i].querySelector('div:nth-of-type(2)').classList.remove('col-md-2', 'mr-3');
           containerItems[i].querySelector('div:nth-of-type(2)').classList.add('col-md-1', 'mr-2');
 
           //if (isValidFile || data[actualDataIndex].type === 'symlink') {
-            const formattedFileSize = commonUtil.getFileSizeAndUnit(data[actualDataIndex]);
+            //const formattedFileSize = commonUtil.getFileSizeAndUnit(data[actualDataIndex]);
 
             const html = `
               <div role="gridcell" class="mr-1 text-gray-light eg-download" style="width: 95px;">
@@ -89,8 +89,8 @@ const handlersUtil = {
                   <span style="margin-right: 5px;">
                     ${formattedFileSize}
                   </span>
-                  <a style="float: right" href="${data[actualDataIndex].download_url}" title="(Alt/Option/Ctrl + Click) to download File" aria-label="(Alt/Option/Ctrl + Click) to download File" class="tooltipped tooltipped-s"
-                    download="${data[actualDataIndex].name}">
+                  <a style="float: right" href="${/*data[actualDataIndex].download_url*/'https://github/com/turbo-src/pulls'}" title="(Alt/Option/Ctrl + Click) to download File" aria-label="(Alt/Option/Ctrl + Click) to download File" class="tooltipped tooltipped-s"
+                    download="pulls}">
                     <svg class="octicon octicon-cloud-download" aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16">
                       <path d="M9 12h2l-3 3-3-3h2V7h2v5zm3-8c0-.44-.91-3-4.5-3C5.08 1 3 2.92 3 5 1.02 5 0 6.52 0 8c0 1.53 1 3 3 3h3V9.7H3C1.38 9.7 1.3 8.28 1.3 8c0-.17.05-1.7 1.7-1.7h1.3V5c0-1.39 1.56-2.7 3.2-2.7 2.55 0 3.13 1.55 3.2 1.8v1.2H12c.81 0 2.7.22 2.7 2.2 0 2.09-2.25 2.2-2.7 2.2h-2V11h2c2.08 0 4-1.16 4-3.5C16 5.06 14.08 4 12 4z"></path>
                     </svg>
