82fdf994583349e3e8f843e5539b6b922eac47f5
7db9a <repo.7db9a@gmail.com>
2021-11-27 13:45:19 -0500

    Add react component in popup.js.

--- popup.js
+++ popup.js
@@ -1,5 +1,25 @@
-/* global chrome */
+'use strict';
 
-document.getElementById('settings-btn').addEventListener('click', function() {
-  chrome.runtime.openOptionsPage();
-});
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
