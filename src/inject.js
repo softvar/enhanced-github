f50f96d3e180c78cfee8839bb77f4462fdd4ded3
7db9a <repo.7db9a@gmail.com>
2021-12-02 04:15:42 -0500

    Successfully make vote request to server.

--- src/inject.js
+++ src/inject.js
@@ -8,6 +8,26 @@ const messageListenerUtil = require('./utils/messageListenerUtil');
 const domUtil = require('./utils/domUtil');
 const storageUtil = require('./utils/storageUtil');
 const CommonEnum = require('./enums/CommonEnum');
+const superagent = require('superagent');
+function post() {
+  superagent
+    .post('http://localhost:4000/graphql')
+    .send(
+      //{ query: '{ name: 'Manny', species: 'cat' }' }
+      //{ query: '{ newPullRequest(pr_id: "b", contributor_id: "2", side: 1) { vote_code } }' }
+      //{ query: '{ getVote(pr_id: "a", contributor_id: 1) {side} }' }
+      //{ query: '{ getVoteAll(pr_id: "a") { vote_code } }' }
+      { query: '{ getVoteEverything }' }
+      //{ query: '{ setVote(pr_id: "b" contributor_id: "1", side: 0 ) { vote_code } }' }
+      //{ query: '{ setVote(pr_id: "c" contributor_id: "2", side: 1 ) { vote_code }' }
+    ) // sends a JSON post body
+    .set('accept', 'json')
+    .end((err, res) => {
+      // Calling the end function will send the request
+    });
+}
+
+var voted = false;
 
 (function() {
   window.enhancedGithub = {
@@ -28,9 +48,10 @@ const CommonEnum = require('./enums/CommonEnum');
           // maybe gets vote side from chrome.storage that onPathContentFetched saved.
 
           // const vote = chrome.storage("vote")
-          vote_result = getVote();
+          vote_result = post();
 
-          if (vote_result !== "hello") {
+          voted = true;
+
 
             // if status is good, continue.
 
@@ -40,13 +61,12 @@ const CommonEnum = require('./enums/CommonEnum');
           //} else {
           //   // maybe remove from chrome.storage the last vote.
           //}
-          }
 
         },
         false
       );
 
-      if (vote_result !== "hello") {
+      //if (voted === true) {
           messageListenerUtil.addListners();
 
           chrome.storage.sync.get(
@@ -60,7 +80,7 @@ const CommonEnum = require('./enums/CommonEnum');
               domUtil.addRepoData();
             }
           );
-      }
+      //}
     }
   }, 10);
 })();
