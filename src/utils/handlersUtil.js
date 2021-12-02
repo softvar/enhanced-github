42c4abf665ae9f9565912813dbfab1558cc5182c
7db9a <repo.7db9a@gmail.com>
2021-12-02 06:13:40 -0500

    Messed around with injected html.

--- src/utils/handlersUtil.js
+++ src/utils/handlersUtil.js
@@ -10,8 +10,8 @@ const handlersUtil = {
       <button aria-label="Copy file contents to clipboard" class="js-file-clipboard btn btn-sm BtnGroup-item file-clipboard-button tooltipped tooltipped-s js-enhanced-github-copy-btn" data-copied-hint="Copied!" type="button" click="selectText()" data-clipboard-target="tbody">
         Copy File
       </button>
-      <a href="https://github/come/turbo-src/pulls" download="pulls"
-        aria-label="(Alt/Option/Ctrl + Click) to download File" class="js-file-download btn btn-sm BtnGroup-item file-download-button tooltipped tooltipped-s">
+      <a download="pulls"
+        aria-label="(Alt/Option/Ctrl + Click) to download File" class="vote-action btn btn-sm BtnGroup-item file-download-button tooltipped tooltipped-s">
         <span style="margin-right: 5px;">${formattedFileSize}</span>
         <svg class="octicon octicon-cloud-download" aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16">
           <path d="M9 12h2l-3 3-3-3h2V7h2v5zm3-8c0-.44-.91-3-4.5-3C5.08 1 3 2.92 3 5 1.02 5 0 6.52 0 8c0 1.53 1 3 3 3h3V9.7H3C1.38 9.7 1.3 8.28 1.3 8c0-.17.05-1.7 1.7-1.7h1.3V5c0-1.39 1.56-2.7 3.2-2.7 2.55 0 3.13 1.55 3.2 1.8v1.2H12c.81 0 2.7.22 2.7 2.2 0 2.09-2.25 2.2-2.7 2.2h-2V11h2c2.08 0 4-1.16 4-3.5C16 5.06 14.08 4 12 4z"></path>
@@ -46,7 +46,7 @@ const handlersUtil = {
             const html = `
               <div id="gridcell" class="mr-2 text-gray-light eg-download" style="width: 40px;">
                 <span class="css-truncate css-truncate-target d-block">
-                  <a style="float: right" href="${/*data[actualDataIndex].download_url*/'https://github.com/turbo-src'}" title="(Alt/Option/Ctrl + Click) to download File" aria-label="(Alt/Option/Ctrl + Click) to download File" class="tooltipped tooltipped-s"
+                  <a style="float: right" title="(Alt/Option/Ctrl + Click) to download File" aria-label="(Alt/Option/Ctrl + Click) to download File" class="tooltipped tooltipped-s"
                     }">
                     <svg class="octicon octicon-cloud-download" aria-hidden="true" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">  <image id="image0" width="24" height="24" x="0" y="0"
                     href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAQAAABKfvVzAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
