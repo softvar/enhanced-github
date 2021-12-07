17c42ff42925a881a191aaf73c5f606fe10cb44f
7db9a <repo.7db9a@gmail.com>
2021-12-06 21:33:00 -0500

    Move svg into a variable.

--- src/utils/handlersUtil.js
+++ src/utils/handlersUtil.js
@@ -35,16 +35,7 @@ const handlersUtil = {
 
       // Get contributor_id from chain web wallet extension
       const contributor_id = "turbo-src-dev";
-
-      for (var i = startIndex; i < containerItems.length; i++) {
-            var issue_id = containerItems[i].getAttribute('id');
-
-            const voteYesHtml = `
-              <div id="gridcell" class="mr-2 text-gray-light eg-download style="width: 24px;">
-                <span class="css-truncate css-truncate-target d-block">
-                  <a style="float: right" title="Vote Yes" aria-label="(Alt/Option/Ctrl + Click) to download File" class="tooltipped tooltipped-s"
-                    }">
-                    <svg class="octicon octicon-cloud-download" aria-hidden="true" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">  <image id="voteYes" width="24" height="24" x="0" y="0" issue_id="${issue_id}" contributor_id="${contributor_id}"
+      const svg_data = `
                     href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAQAAABKfvVzAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
                 AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZ
                 cwAADsQAAA7EAZUrDhsAAAAHdElNRQflCxQEJyIqWooVAAABsklEQVQ4y53UwUtUURgF8J+jm1rI
@@ -57,7 +48,18 @@ const handlersUtil = {
                 bOtVhesUFa4/GiPXicbF8E1cHr6BvnjP5/HetGUfdzz0yAiCN5Yd/NcD6hXuwBqCYxlKSsjy2Vpv
                 94G+M843gd2rmsC12sxf+fsRd+kA99MAAAAldEVYdGRhdGU6Y3JlYXRlADIwMjEtMTEtMjBUMDQ6
                 Mzk6MzQrMDA6MDCigy1GAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIxLTExLTIwVDA0OjM5OjM0KzAw
-                OjAw096V+gAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAAASUVORK5CYII=" />
+                OjAw096V+gAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAAASUVORK5CYII=" />`
+
+      for (var i = startIndex; i < containerItems.length; i++) {
+            var issue_id = containerItems[i].getAttribute('id');
+
+            const voteYesHtml = `
+              <div id="gridcell" class="mr-2 text-gray-light eg-download style="width: 24px;">
+                <span class="css-truncate css-truncate-target d-block">
+                  <a style="float: right" title="Vote Yes" aria-label="(Alt/Option/Ctrl + Click) to download File" class="tooltipped tooltipped-s"
+                    }">
+                    <svg class="octicon octicon-cloud-download" aria-hidden="true" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">  <image id="voteYes" width="24" height="24" x="0" y="0" issue_id="${issue_id}" contributor_id="${contributor_id}"` +
+                    svg_data + `
                 </svg
                   </a>
                 </span>
@@ -69,20 +71,8 @@ const handlersUtil = {
                 <span class="css-truncate css-truncate-target d-block">
                   <a style="float: right" title="Vote No" aria-label="(Alt/Option/Ctrl + Click) to download File" class="tooltipped tooltipped-s"
                     }">
-                    <svg class="octicon octicon-cloud-download" aria-hidden="true" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">  <image id="voteNo" width="24" height="24" x="0" y="0" issue_id="${issue_id}" contributor_id="${contributor_id}"
-                    href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAQAAABKfvVzAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
-                AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZ
-                cwAADsQAAA7EAZUrDhsAAAAHdElNRQflCxQEJyIqWooVAAABsklEQVQ4y53UwUtUURgF8J+jm1rI
-                GwlCGMKWgo9qN5tajkj/gNvciBtFiYImaDsLNwW6iZbuAmFAhpbpqoVEIKO7QoRWr4kKQeW922Je
-                zqhPlN7dvMs9595zz3fuR9EX27AhLloa6vsfNeVQx5gF47jrta9G3NTyvYhcF2SOpEI+0nxWL4JX
-                tE+BQdpHC9oq/2CDp/CnHhvADx+89c57nxy67QZuGbTjV0/7C22ZINNUFYlNmRKLVDXzlba60S5h
-                Jj84s6qspiVx4kSipaZsVZYjZrqE6VxvU9mcTp/24Kc5Zc38XtNdwqQjQaKqdg4eBB01VYngyCQl
-                jBnCR3sWRRfciyzas4khY92qdu1cEkvO7J1atSKTiC3l9sYlDePI7KsYxo5dkFnx3DepYRX7Moxr
-                lM4JONYw7QtWvPTEqzPhUSRp2z0PPBOZ90cQJCZ6kmBWKlgXaQmCz+5jIYcHLZF1QWr2Mlu3Lftd
-                bOtVhesUFa4/GiPXicbF8E1cHr6BvnjP5/HetGUfdzz0yAiCN5Yd/NcD6hXuwBqCYxlKSsjy2Vpv
-                94G+M843gd2rmsC12sxf+fsRd+kA99MAAAAldEVYdGRhdGU6Y3JlYXRlADIwMjEtMTEtMjBUMDQ6
-                Mzk6MzQrMDA6MDCigy1GAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIxLTExLTIwVDA0OjM5OjM0KzAw
-                OjAw096V+gAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAAASUVORK5CYII=" />
+                    <svg class="octicon octicon-cloud-download" aria-hidden="true" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">  <image id="voteNo" width="24" height="24" x="0" y="0" issue_id="${issue_id}" contributor_id="${contributor_id}"` +
+                    svg_data + `
                 </svg
                   </a>
                 </span>
