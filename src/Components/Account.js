e9cbf5646054da78dc92ddcfa4beaa000fb5363e
Jeffrey Wood <jwfirenze@gmail.com>
2022-09-07 17:17:15 -0400

    token amount rendering in account page

--- src/Components/Account.js
+++ src/Components/Account.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Settings from './Settings';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { postGetContributorTokenAmount } from '../requests';
 
 export default function Account() {
   const user = useSelector(state => state.auth.user);
   const navigate = useNavigate();
   let name = user?.name;
   let username = user?.login;
-  let tokens = user?.tokens || `No tokens.`;
+  let [tokenAmount, setTokenAmount] = useState('');
   let avatar = user?.avatar_url || null;
+  let [repo, setRepo] = useState('');
+  let [owner, setOwner] = useState('');
+
+  useEffect(() => {
+    chrome.storage.local.get(['repo'], data => setRepo(data.repo));
+    chrome.storage.local.get(['owner'], data => setOwner(data.owner));
+  }, []);
+
+  useEffect(() => {
+    const getTokenAmount = async () => {
+      await postGetContributorTokenAmount(owner, repo, '', user.ethereumAddress, '').then(res => setTokenAmount(res));
+    };
+    getTokenAmount();
+  });
+
   return (
     <div className="content flex-col">
       <span className="bigText">Account</span>
@@ -25,7 +41,7 @@ export default function Account() {
               </a>
             </li>
             <li className="secondary tokensLine">
-              {tokens} {user?.tokens ? null : <span onClick={() => navigate('/onboard')}>Click to create some.</span>}
+              <span>{` ${tokenAmount || 0} ${repo}`}</span> tokens
             </li>
           </ul>
         </span>
