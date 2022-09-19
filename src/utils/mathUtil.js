ce62f087822482c33b03ed3c8825898cf0bdc39e
7db9a <repo.7db9a@gmail.com>
2022-09-19 10:31:32 -0400

    feat: show quorum percent on vote button.

--- src/utils/mathUtil.js
+++ src/utils/mathUtil.js
@@ -1,5 +1,5 @@
 const mathUtil = {
-  yesVotePercentToMergeInteger: function(voteYesTotal) {
+  votePercentToMergeInteger: function(voteYesTotal) {
     voteYesTotal = Number(voteYesTotal);
     const quorum = 0.5 //getRepo
     const majority = 0.5 // implicit, must be greater than
@@ -7,20 +7,20 @@ const mathUtil = {
     // majority + 1 vote.
     const yesVotesMinimumToMerge = quorum*supply*majority + 1 
     
-    var yesVotePercentToMergeInteger = 100*((voteYesTotal / yesVotesMinimumToMerge))
+    var votePercentToMergeInteger = 100*((voteYesTotal / yesVotesMinimumToMerge))
     
     // Show 1 decimal if less than 10%. Greater, round to nearest integer.
-    if (yesVotePercentToMergeInteger < 10) {
-      return yesVotePercentToMergeInteger.toFixed(1)
+    if (votePercentToMergeInteger < 10) {
+      return votePercentToMergeInteger.toFixed(1)
      // So it doesn't say "100%" when it's actually 99.9% or something.
     // Untested
-    } else if (yesVotePercentToMergeInteger > 99 && yesVotePercentToMergeInteger < 100) {
-      return yesVotePercentToMergeInteger.toFixed(1)
-    } else if (yesVotePercentToMergeInteger > 100) {
+    } else if (votePercentToMergeInteger > 99 && votePercentToMergeInteger < 100) {
+      return votePercentToMergeInteger.toFixed(1)
+    } else if (votePercentToMergeInteger > 100) {
       return 100
     } else {
       // Logically, only if exactly 100.
-      return Math.round(yesVotePercentToMergeInteger)
+      return Math.round(votePercentToMergeInteger)
     }
   }
 };
