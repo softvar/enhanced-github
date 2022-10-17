22e942f95d5cb57e86b9952c884607cfa5ba8fcc
Jeffrey Wood <jwfirenze@gmail.com>
2022-10-17 17:49:23 -0400

    fetching ok, not setting to variable in global scope

--- src/popup.js
+++ src/popup.js
@@ -1,20 +1,29 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 import App from './App.js';
+const commonUtil = require('./utils/commonUtil');
 
 const e = React.createElement;
 var issueId = 'waiting...';
-chrome.tabs.query(
-                 {active:true, lastFocusedWindow:true},
-                 tabs=>{
-                            const tab=tabs[0];
-                            console.log("URL:", tab)
-                            }
-                             )
 
+
+var currentRepo = {}
+
+chrome.tabs.query(
+{active:true, lastFocusedWindow:true},
+tabs=>{
+const tab=tabs[0];
+const pathnames = tab.url.split('/');
+const user = pathnames[3];
+const repo = pathnames[4];
+
+fetch(`https://api.github.com/repos/${user}/${repo}`).then((response) => response.json())
+.then((data) => currentRepo = data)
+}
+)
 
 document.addEventListener('DOMContentLoaded', function() {
   const domContainer = document.querySelector('#rootcontainer');
 
-  ReactDOM.render(e(App), domContainer);
+  ReactDOM.render(e(App,{currentRepo: currentRepo}), domContainer);
 });
