1a03e3e09a7fa7285e22f0e00c23bad96f1c423f
Jeffrey Wood <jwfirenze@gmail.com>
2022-10-25 10:18:31 -0400

    login page fields removed

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -8,23 +8,6 @@ export default function Auth() {
       <div className="auth">
         <form>
           <Login />
-          <span className="items-center bold light">
-            <div className="line"></div>
-            or
-            <div className="line"></div>
-          </span>
-          <span className="items-center">
-            <label htmlFor="name">Continue with just your name instead:</label>
-          </span>
-
-          <input type="text" name="name"></input>
-          <span className="items-center">
-            <Link to="/onboard">
-              <button type="button" className="startButton">
-                Start
-              </button>
-            </Link>
-          </span>
         </form>
       </div>
     </div>
