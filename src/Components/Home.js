cca1b5cf1d0574e647bcff04ec2898262c0da765
Jeffrey Wood <jwfirenze@gmail.com>
2022-10-25 11:01:30 -0400

    home page text not so large

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -75,13 +75,13 @@ export default function Home() {
             </span>
           </div>
           {tokenized ? 
-            <span>
+            <span className="votePower">
             {tokenAmount === 0 ?
               'You do not have votepower in this project.'
               : `${tokenAmount} votepower`}
             </span>
             : null}
-          <div className="notOnTurbosrc">
+          <div className="votePower">
             {tokenized ? `` : `${owner}/${repo} is not on turbosrc.`}
           </div>
         </section>
