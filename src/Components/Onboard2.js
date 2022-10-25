dd7bd188427432e83def6f79ac834d5c5b8b5480
Jeffrey Wood <jwfirenze@gmail.com>
2022-10-25 11:27:09 -0400

    excess wording removed

--- src/Components/Onboard2.js
+++ src/Components/Onboard2.js
@@ -192,10 +192,6 @@ export default function Onboard2() {
     return (
       <div className="content">
         <div className="onboard">
-          <span className="">
-            <h1>Tokenize This Repository</h1>
-          </span>
-          <span>Tokenizing {repo} will automatically create 1000000 tokens.</span>
 
           <form name="create">
             <span>{errorText}</span>
