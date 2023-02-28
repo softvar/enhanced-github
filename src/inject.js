5e09f21d5f361cd796a2a8b4f7cfdc6c6480e79f
7db9a <>
2023-02-28 12:52:14 -0500

    Change url to alternative network.

--- src/inject.js
+++ src/inject.js
@@ -37,8 +37,8 @@ const { postSetVote,
         getGitHubPullRequest
       } = require('./requests')
 
-const port = "http://localhost:4000";
-//const port = "https://turbosrc-service.fly.dev"
+//const port = "http://localhost:4000";
+const port = "turbosrc-reibase-alternate-service.fly.dev"
 
 var isRepoTurboSrcToken = false;
 
