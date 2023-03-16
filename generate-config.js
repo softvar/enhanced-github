5833898b81263f0e1381fe427184455d50f1c469
7db9a <>
2023-03-16 11:37:05 -0400

    Add messages when generate-config is ran.

--- generate-config.js
+++ generate-config.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Load the desired config file based on the environment
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.TSRC_EXT_NODE_ENV || 'devLocal';
 const configFilePath = path.join(__dirname, `config.${env}.json`);
 const config = require(configFilePath);
 
@@ -10,3 +10,11 @@ const config = require(configFilePath);
 const configJs = `const CONFIG = ${JSON.stringify(config)};
 module.exports = CONFIG;`;
 fs.writeFileSync(path.join(__dirname, 'src', 'config.js'), configJs);
+
+console.log()
+console.log('\033[1mturbosrc build mode:\033[0m \033[33m%s\033[0m', env);
+console.log()
+console.log('config file:', configFilePath)
+console.log('# Begin config file contents')
+console.log(configJs)
+console.log('# End')
