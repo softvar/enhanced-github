43854e1db55a3158a78963596a6fe986d2755bfd
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2023-05-19 12:59:22 -0400

    refactor: vote button old code removed

--- src/Components/VoteButton.js
+++ src/Components/VoteButton.js
@@ -1,14 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { postSetVote } from '../requests';
 import styled from 'styled-components';
-import { set } from '../utils/storageUtil';
-//    background-color: ${props => props.$option ? "#038800" : "#D33131"};
-//${props.$option === 'no' ? 'background-color: #D33131;' : ''} was in line 19
+
 const Vote = styled.button`
     @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'); 
-
-    
-
     ${props => props.$voted == true && `
     /* Disable the opposite button when a vote has been cast */
     ${props.$chosenSide === 'yes' && props.$option === 'no' ? 'background-color: #B7B7B7;' : ''}
@@ -23,15 +18,12 @@ const Vote = styled.button`
     /* Disable the opposite button when a vote has been cast */
     ${props.$chosenSide === 'yes' && props.$option === 'yes' ? 'background-color: #038800;' : ''}
     ${props.$chosenSide === 'no' && props.$option === 'no' ? 'background-color: #038800;' : ''}
-    
   `}
 
   ${props => !props.$voted && `
     /* Styles for when no vote has been cast */
     ${props.$option === 'yes' ? 'background-color: #038800;' : 'background-color: #D33131;'}
-    
   `}
-
     border: none;
     color: white; 
     padding: 10px 34px; /* Some padding */
@@ -41,8 +33,6 @@ const Vote = styled.button`
     font-family: 'Inter', sans-serif;
     font-size:24px;
     border-radius: 5px;
-
-    
     `;
 
     const SelectedVote = styled(Vote)`
@@ -50,7 +40,6 @@ const Vote = styled.button`
         ${props => !props.$voted && `
           /* Styles for when no vote has been cast */
           ${props.$option === 'yes' ? 'background-color: #038800;' : 'background-color: #D33131;'}
-          
         `}
       }
     `;
@@ -64,36 +53,11 @@ function VoteButton(props) {
   const [chosenSide, setChosenSide] = useState(props.chosenSide);
   const [option, setOption] = useState(props.side);
 
-
-  if (voted === 'pull' && props.issueID === lastIssueId) {
-    return 'Verifying. This may take a few a couple minutes...';
-  }
-  if (voted === 'problem' && props.issueID === lastIssueId) {
-    return 'Something went wrong';
-  }
-  if (voted === 'notOnGithub' && props.issueID === lastIssueId) {
-    return "Pull request isn't valid on github (path to fork doesn't exist).";
-  }
-  if (voted === 'done' && props.issueID === lastIssueId) {
-    return (
-      <div>
-        user: {props.user} <br />
-        repo: {props.repo} <br />
-        issue_id: {props.issueID} <br />
-        contributor: {props.contributorName} <br />
-        side: {props.side} <br />
-      </div>
-    );
-  }
-
   useEffect(() => {
     if (props.disabled === true || props.voted == true && props.chosenSide != props.side) {
       setDisabledButton(true);
     }
   }, [props.disabled, props.voted, props.chosenSide, props.side]);
-
-  console.log(disabledButton + " OIHWEIUHGAEIURHGIUEHRGOIAEHRGIUOAHGIUAHRGIUAHERGUI;AEHRGIUAEHRGI;A");
-
 
   return props.voted && props.chosenSide === props.side ? (
     <SelectedVote
