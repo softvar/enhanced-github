eb1fb2b192df5413d7c5b2c0519e15126f0d9a45
Cristian <125297328+ramirc5@users.noreply.github.com>
2023-07-12 15:12:42 -0700

    Layout updated, currently working on getting the click to copy string to clipboard functionality to work

--- src/Components/Extension/Account/Account.js
+++ src/Components/Extension/Account/Account.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Settings from './Settings';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { postGetVotePowerAmount } from '../../../requests';
 import useCommas from '../../../hooks/useCommas';
+import styled from 'styled-components';
+
 export default function Account() {
   const user = useSelector(state => state.auth.user);
   const repo = useSelector(state => state.repo.name);
@@ -15,6 +17,68 @@ export default function Account() {
   let [tokenAmount, setTokenAmount] = useState('');
   let avatar = user?.avatar_url || null;
 
+  const Profile = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    `;
+
+    const Username = styled.span`
+      color: #6A6868;
+      font-size: 15px;
+      font-weight: 600;
+    `;
+
+    const ProfilePicture = styled.img`
+      height: 5rem;
+      width: 5rem;
+      display: block;
+      object-fit: cover;
+      border-radius: 50%;
+      margin: 5px;
+      background-color: var(--lightbg);
+    `;
+
+    const AccountContent = styled.div`
+    width: 24rem;
+    height: 27rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    overflow-y: auto;
+    overflow-x: hidden;
+    padding: 0.5rem;
+    `;
+
+    const AccountHeader = styled.div`
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+    `;
+
+    const AccountAddress = styled.div`
+      width: 260px;
+    `;
+
+    const Address = styled.div`
+      display: flex;
+      flex-direction: column;
+      `;
+
+      const AddressHeading = styled.span`
+        color: #6A6868;
+        font-size: 11px;
+        font-weight: 600;
+      `;
+
+      const EthAddress = styled.span`
+        color: black;
+        font-size: 10px;
+        font-weight: 600;
+      `;
+
+
   useEffect(() => {
     const getTokenAmount = async () => {
       await postGetVotePowerAmount(owner, repo, '', user.ethereumAddress, '', user.token)
@@ -22,14 +86,25 @@ export default function Account() {
         .then(tokens => setTokenAmount(tokens));
     };
     getTokenAmount();
+    console.log(user, 'user', repo, 'repo', owner, 'owner', name, 'name', username, 'username');
   });
 
   return (
-    <div className="content flex-col">
-      <span className="bigText">Account</span>
-      <div className="profile items-center">
-        <img src={avatar} className="profilePicture" />
-        <span>
+    <AccountContent>
+      <AccountHeader>
+        <Profile>
+          <ProfilePicture src={avatar}/>
+          <Username>
+            <a href={user?.html_url} target="_blank">
+              {username}
+            </a>
+          </Username>
+        </Profile>
+        <AccountAddress>
+          <Address>
+            <AddressHeading>Address</AddressHeading>
+            <EthAddress>{user?.ethereumAddress}</EthAddress>
+          </Address>
           <ul>
             <li className="bold">{name}</li>
             <li className="secondary githubLine">
@@ -42,10 +117,10 @@ export default function Account() {
               <span>{` ${tokenAmount || 0} ${repo}`}</span> VotePower
             </li>
           </ul>
-        </span>
-      </div>
+        </AccountAddress>
+      </AccountHeader>
 
       <Settings />
-    </div>
+    </AccountContent>
   );
 }
